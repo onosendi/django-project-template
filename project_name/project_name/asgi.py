13a67ef9ac5dac70d1a5f46290061f495bb5db87
Daniel Lindegren <onosendi@protonmail.com>
2021-01-05 09:16:39 -0700

    asgi/wsgi: fix settings strings

--- project_name/project_name/asgi.py
+++ project_name/project_name/asgi.py
@@ -4,7 +4,7 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
-    '{{ project_name }}.settings',
+    '{{ project_name }}.settings.production',
 )
 
 application = get_asgi_application()
--- project_name/project_name/wsgi.py
+++ project_name/project_name/wsgi.py
@@ -4,7 +4,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
-    '{{ project_name }}.settings',
+    '{{ project_name }}.settings.production',
 )
 
 application = get_wsgi_application()
